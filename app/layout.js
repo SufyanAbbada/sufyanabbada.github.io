e10311cf2df941307acd444bafb3108e740e40c7
SufyanAbbada <sufyan.abada.sa@gmail.com>
2023-08-22 06:38:43 +0500

    Meta Tag implemented with OG Styles

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import "./globals.css";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,7 +7,35 @@ export const metadata = {
   title: "Sufyan Abbada",
   description:
     "Full-stack developer with expertise in top JS frameworks. Crafting user-centric solutions with a global outlook, prioritizing performance and scalability.",
-  author: "Sufyan Abbada",
+  generator: "Next.js",
+  applicationName: "Sufyan's Portfolio",
+  keywords: [
+    "Next.js",
+    "React",
+    "JavaScript",
+    "Portfolio",
+    "Web Development",
+    "Software Engineer",
+    "Full Stack Engineer",
+  ],
+  authors: [{ name: "Sufyan Abbada" }],
+  creator: "Sufyan Abbada",
+  publisher: "Sufyan Abbada",
+  category: "technology",
+
+  openGraph: {
+    title: "Sufyan's Professional Portfolio",
+    type: "profile",
+    images: [
+      {
+        url: `${process.env.CLOUDINARY_URL}/SufyanAbbadaFrontImageFB_qoi9hm.png`,
+      },
+    ],
+    url: "https://example.com/path-to-article", // Todo: Change the URL to deployed URL afterwards
+    description:
+      "Full-stack developer with expertise in top JS frameworks. Crafting user-centric solutions with a global outlook, prioritizing performance and scalability.",
+    site_name: "Sufyan Abbada",
+  },
 };
 
 export default function RootLayout({ children }) {
